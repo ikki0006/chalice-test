73c9a01bd5bcc44efdcde5fa818a96e4fb22056f
s-kazuki <ikki0006@gmail.com>
2024-05-22 21:06:45 +0900

    変更点がわかるように

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def scheduled_event1(event):
     print("Scheduled event: %s" % event.to_dict())
     return {"hello": "world"}
 
-@app.schedule("rate(1 hour)")
+@app.schedule("rate(2 hour)")
 def scheduled_event2(event):
     print("Scheduled event: %s" % event.to_dict())
     return {"hello": "world2"}
